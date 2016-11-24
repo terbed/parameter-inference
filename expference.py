1437c67c7f03fce895944e50e8b60827a2663986
Terbe Dániel <terbed@gmail.com>
2016-11-24 20:14:43 +0100

    New results and Ipythone notebooks

--- expference.py
+++ expference.py
@@ -23,12 +23,6 @@ from module.plot import plot3d
 Ra = RandomVariable(name='Ra', range_min=100., range_max=380., resolution=100, mean=157.362128223, sigma=50)
 gpas = RandomVariable(name='gpas', range_min=0.00030, range_max=0.0005, resolution=100, mean=0.000403860792541, sigma=0.00005)
 
-# --- Load NEURON morphology
-h('load_file("/Users/Dani/TDK/parameter_estim/exp/morphology_131117-C2.hoc")')
-# Set the appropriate "nseg"
-for sec in h.allsec():
-    sec.Ra = Ra.range_max
-h('forall {nseg = int((L/(0.1*lambda_f(100))+.9)/2)*2 + 1}')  # If Ra_max = 105 dend.nseg = 21 and soma.nseg = 1
 
 
 # --- Load experimental trace
@@ -36,9 +30,16 @@ experimental_trace = genfromtxt("/Users/Dani/TDK/parameter_estim/exp/resampled_e
 t = experimental_trace[:, 0]
 exp_v = experimental_trace[:, 1]
 
+# --- Load NEURON morphology
+h('load_file("/Users/Dani/TDK/parameter_estim/exp/morphology_131117-C2.hoc")')
+# Set the appropriate "nseg"
+for sec in h.allsec():
+    sec.Ra = Ra.range_max
+h('forall {nseg = int((L/(0.1*lambda_f(100))+.9)/2)*2 + 1}')  # If Ra_max = 105 dend.nseg = 21 and soma.nseg = 1
+
 # --- Load inverse covariant matrix - [Generate inverse covariant matrix]
 print "Loading inverse covariance matrix..."
-invcovmat = genfromtxt('/Users/Dani/TDK/parameter_estim/exp/inv_covmat_1_0.01.txt')
+invcovmat = genfromtxt('/Users/Dani/TDK/parameter_estim/exp/invcovmat2.txt')
 print "Done..."
 
 # --- Inference
@@ -57,20 +58,3 @@ print Ra_gpas
 plot3d(Ra, gpas, Ra_gpas.likelihood, 'likelihood', "/Users/Dani/TDK/parameter_estim/exp/out2/")
 plot3d(Ra, gpas, Ra_gpas.posterior, 'Posterior', "/Users/Dani/TDK/parameter_estim/exp/out2/")
 
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-x, y = np.meshgrid(gpas.values, Ra.values)
-ax.plot_surface(x, y, Ra_gpas.posterior, rstride=8, cstride=8, alpha=0.3)
-cset = ax.contour(x, y, Ra_gpas.posterior, zdir='z',  offset=-0, cmap=CM.coolwarm)
-cset = ax.contour(x, y, Ra_gpas.posterior, zdir='x', offset=gpas.range_min, cmap=CM.coolwarm)
-cset = ax.contour(x, y, Ra_gpas.posterior, zdir='y', offset=Ra.range_max, cmap=CM.coolwarm)
-ax.set_title('Posterior')
-ax.set_xlabel('gpas [uS]')
-ax.set_ylabel('Ra [ohm cm]')
-plt.savefig("/Users/Dani/TDK/parameter_estim/exp/out/posterior_0.1c"+str(Ra.resolution)+".png")
-
-
-
-
-plt.show()
