9cb9061817eec12532b6a2231931b62e79534e75
Terbe Dániel <terbed@gmail.com>
2016-11-27 22:32:35 +0100

    list comprehension

--- module/prior.py
+++ module/prior.py
@@ -12,18 +12,24 @@ def normal(vec, mean, sigma):
     """Returns a normal distribution as a numpy array
         equal to map(partial(normal_val, mean=mean, sigma=sigma), vec)
     """
-
-    return [partial(normal_val, mean=mean, sigma=sigma)(x) for x in vec]
+    normal_val_map = partial(normal_val, mean=mean, sigma=sigma)
+    return [normal_val_map(x) for x in vec]
 
 
 def normal2d(x_mean, x_sigma, x_values, y_mean, y_sigma, y_values):
-    """Returns a two dimensional normal distribution as a numpy array"""
-    prior = np.zeros((len(x_values), len(y_values)))
-    for i, x in enumerate(x_values):
-        for j, y in enumerate(y_values):
-            prior[i, j] = normal_val(x_mean, x_sigma, x) * normal_val(y_mean, y_sigma, y)
+    """Returns a two dimensional normal distribution as a numpy array
 
-    return prior
+    The solution is equivalent to:
+        prior = np.zeros((len(x_values), len(y_values)))
+        for i, x in enumerate(x_values):
+            for j, y in enumerate(y_values):
+                prior[i, j] = normal_val(x_mean, x_sigma, x) * normal_val(y_mean, y_sigma, y)
+        return prior
+    """
+
+    normal_val_xmap = partial(normal_val, mean=x_mean, sigma=x_sigma)
+    normal_val_ymap = partial(normal_val, mean=y_mean, sigma=y_sigma)
+    return [[normal_val_xmap(x) * normal_val_ymap(y) for y in y_values] for x in x_values]
 
 
 def normal_nd(*priors):
@@ -57,17 +63,37 @@ if __name__ == "__main__":
     from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
     x1 = np.linspace(-100, 100, 20)
-    y1 = normal(0, 18, x1)
+    y1 = normal(x1, 0, 18)
 
-    x2 = np.linspace(-50, 50, 30)
-    y2 = normal(0, 3, x2)
+    x2 = np.linspace(-50, 50, 300)
+    y2 = normal(x2, 0, 3)
 
-    x3 = np.linspace(-30, 30, 40)
-    y3 = normal(0, 6, x3)
+    x3 = np.linspace(-30, 30, 400)
+    y3 = normal(x3, 0, 6)
 
     x4 = np.linspace(-10, 10, 50)
-    y4 = normal(0, 2, x4)
+    y4 = normal(x4, 0, 2)
 
+    plt.figure()
+    plt.plot(x1,y1)
+    plt.show()
+
+    z = normal2d(0,3,x2,0,4,x3)
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    x, y = np.meshgrid(x3, x2)
+    ax.plot_surface(x, y, z, rstride=8, cstride=8, alpha=0.3)
+    cset = ax.contour(x, y, z, zdir='z', offset=-0, cmap=CM.coolwarm)
+    cset = ax.contour(x, y, z, zdir='x', offset=0.00004, cmap=CM.coolwarm)
+    cset = ax.contour(x, y, z, zdir='y', offset=160, cmap=CM.coolwarm)
+    ax.set_title('Posterior')
+    ax.set_xlabel('x2')
+    ax.set_ylabel('x3')
+    plt.show()
+
+# Test normal_nd
+"""
     y1y2y3 = normal_nd(y1, y2, y3, y4)
 
     y11 = y1y2y3.sum(axis=(1, 2, 3)) * abs(x2[1]-x2[0]) * abs(x3[1]-x3[0]) * abs(x4[1]-x4[0])
@@ -89,3 +115,4 @@ if __name__ == "__main__":
     ax.set_xlabel('x1')
     ax.set_ylabel('x2')
     plt.show()
+    """
