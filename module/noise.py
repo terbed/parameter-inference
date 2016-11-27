81dd4b0c0896b5981e2158144e7823a6c3b1a67f
Terbe Dániel <terbed@gmail.com>
2016-11-27 16:08:35 +0100

    Implement colored noise with generators and list comprehensions

--- module/noise.py
+++ module/noise.py
@@ -26,3 +26,52 @@ def colored(D, lamb, dt, v_vec):
         e_0 = e_next
 
     return np.add(v_vec, noise)
+
+
+# Solve the problem with generators and list comprehension ----------------------------------------------------------
+
+def colored_noise_generator(D, lamb, dt):
+    """
+    An iterable generator function for colored noise.
+
+    :param D: Amplitude of the noise
+    :param lamb: Reciprocal of the characteristic time
+    :param dt: Time step
+    :return : yields the successive value
+    """
+
+    e_0 = None
+    E = exp(-lamb * dt)
+
+    while True:
+        if e_0 is None:
+            # Create the first value
+            n, m = np.random.uniform(0.0, 1.0, 2)
+            e_0 = sqrt(-2 * D * lamb * log(m)) * cos(2 * pi * n)
+            yield e_0
+        else:
+            # Create succession
+            a, b = np.random.uniform(0.0, 1.0, 2)
+            h = sqrt(-2 * D * lamb * (1 - E ** 2) * log(a)) * cos(2 * pi * b)
+            e_next = e_0 * E + h
+            e_0 = e_next
+            yield e_0
+
+
+def colored_vector(D, lamb, dt, vec):
+    """
+    Ads colored noise to the given vector.
+    This function uses the colored_noise_generator() generator function
+
+    :param D: amplitude of the noise
+    :param lamb: reciprocal of the characteristic time
+    :param dt: time step
+    :param vec: the vector to extend with noise
+    :return: the given vector with noise
+    """
+
+    # Create color generator
+    noise_generator = colored_noise_generator(D, lamb, dt)
+
+    # iterate through vec and add noise then return the list
+    return [x + noise_generator.next() for x in vec]
