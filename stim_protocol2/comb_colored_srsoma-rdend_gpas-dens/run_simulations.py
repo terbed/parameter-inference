f2ffa93f4c14ee2362aff1b35d2e1ea27bc00e6c
Terbe Dániel <terbed@gmail.com>
2018-11-08 12:55:50 +0100

    Update run_simulations.py

--- stim_protocol2/comb_colored_srsoma-rdend_gpas-dens/run_simulations.py
+++ stim_protocol2/comb_colored_srsoma-rdend_gpas-dens/run_simulations.py
@@ -122,7 +122,7 @@ for item in hz:
     modell = partial(model, stim=stim)
 
     # Generate synthetic data for each fixed params and given repetition
-    target_traces = more_trace_from_covmat(covmat, model_sd=modell, params=fixed_params, rep=noise_rep)
+    target_traces = more_trace_from_covmat(covmat, model=modell, params=fixed_params, rep=noise_rep)
     print "The shape of target traces: " + str(target_traces.shape)
 
     if __name__ == '__main__':
@@ -139,7 +139,7 @@ for item in duration:
     modell = partial(model, stim=stim)
 
     # Generate synthetic data for each fixed params and given repetition
-    target_traces = more_trace_from_covmat(covmat, model_sd=modell, params=fixed_params, rep=noise_rep)
+    target_traces = more_trace_from_covmat(covmat, model=modell, params=fixed_params, rep=noise_rep)
 
     if __name__ == '__main__':
         run_protocol_simulations_c(model=modell, target_traces=target_traces, inv_covmat=invcovmat, param_set=prior_set,
