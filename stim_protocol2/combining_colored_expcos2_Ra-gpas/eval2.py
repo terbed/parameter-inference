e648d36b1d1b7375b7ec202e355561555207550e
terbed <terbed@gmail.com>
2019-07-13 15:56:12 +0200

    Simulation with new (expcos2) noise model

---
+++ stim_protocol2/combining_colored_expcos2_Ra-gpas/eval2.py
@@ -0,0 +1,30 @@
+from module.evaluate import Evaluation
+
+
+nfp = 10  # Number of fixed parameters
+nr = 30   # Number of repetition
+p_names = ['Ra', 'gpas']
+
+dir = "/Users/Dani/TDK/parameter_estim/stim_protocol2/combining_colored_expcos2_Ra_gpas/"
+subdirs = ["steps/3", "steps/20", "steps/200", "sins/1", "sins/10", "sins/100"]    # subdirectories of the man directory
+comb_lists = [["steps/3", "steps/20", "steps/200"],         # we will combine these protocols
+              ["sins/1", "sins/10", "sins/100"]]
+
+e = Evaluation(nfp, nr, p_names, dir, subdirs, comb_lists)
+
+# 1.) some single result plot
+e.single_result_plot(which=0)
+
+# 2.) likelihood multiplications
+e.likelihood_mult()
+
+# 3.) protocol combinations
+e.combinations(comb_lists, ["steps/comb", "sins/comb"])
+
+# 4.) protocol comparison
+path_to_protocols = ["steps/3", "steps/20", "steps/200",         # we will combine these protocols
+                     "sins/1", "sins/10", "sins/100",
+                     "steps/comb", "sins/comb"]
+xtick_list = ['3ms', '20ms', '200ms', '1Hz', '10Hz', '100Hz', 'steps comb', 'sins comb']
+e.compare_protocols(pathlist=path_to_protocols, xticks=xtick_list)
+
---
+++ stim_protocol2/combining_colored_expcos2_Ra-gpas/run_simulations.py
@@ -0,0 +1,157 @@
+import numpy as np
+from neuron import h, gui
+from module.simulation import real_morphology_model
+from module.probability import ParameterSet, RandomVariable
+from module.noise import more_trace_from_covmat, sampling_from_prior, inv_cov_mat
+from functools import partial
+from module.protocol_test import run_protocol_simulations_c
+import tables as tb
+import time
+
+startTime = time.time()
+
+# SET UP SIMULATION PARAMETERS -------------------------------------------------------------------------------------
+
+hz = [1, 10, 100]
+duration = [3, 20, 200]
+
+p_names = ['Ra', 'gpas']
+p_res = [101, 101]  # Parameters resolution
+p_range = [[40, 160], [0.00004, 0.00016]]  # Fixed range, but "true value" may change!
+p_mean = [100., 0.0001]  # Fixed prior mean
+p_std = [20., 0.00002]  # Fixed prior std
+
+# parameters of the noise
+p = [0.1254, 277., 1073.25, 101.03]
+
+# def aut_corr_func(x):
+#     return noise_lamb*noise_D*np.exp(-noise_lamb*np.abs(x))
+
+
+# # expcos
+# def aut_corr_func(x):
+#     return p[0]*np.exp(-np.abs(x)/p[1])*np.cos(2*np.pi/p[2]*np.abs(x))
+
+
+def aut_corr_func(x):
+    return p[0]*np.exp(-np.abs(x)/p[1])*np.cos(2*np.pi/p[2]*np.abs(x)+p[3])
+
+dt = 0.1
+dur_len = 5002
+dur = dur_len*dt
+t_vec = np.linspace(0, dur, dur_len)
+
+noise_rep = 30  # How many repetition while params are fixed
+fixed_param_num = 10  # The number of fixed parameters sampled from prior
+model = real_morphology_model
+
+# --- Load NEURON morphology
+h('load_file("/Users/Dani/TDK/parameter_estim/exp/morphology_131117-C2.hoc")')
+# Set the appropriate "nseg"
+for sec in h.allsec():
+    sec.Ra = p_range[0][1]
+h('forall {nseg = int((L/(0.1*lambda_f(100))+.9)/2)*2 + 1}')  # If Ra_max = 105 dend.nseg = 21 and soma.nseg = 1
+
+batch_size = 30000                 # Set it to "None" to compute the whole parameter space in one stock
+
+# Set up random seed
+np.random.seed(42)
+
+print "Computing covmat and invcovmat..."
+covmat, invcovmat = inv_cov_mat(aut_corr_func, t_vec)
+print "Inverse covariance matrix is loaded to memory!"
+print invcovmat.shape
+
+
+# END OF SETTING UP SIMULATION PARAMETERS -----------------------------------------------------------------------------
+
+
+
+
+
+
+# Set up parameters using prior information about them (fix the range we are assuming the true parameter)
+prior_params = []
+for idx, item in enumerate(p_names):
+    prior_params.append(RandomVariable(name=item, range_min=p_range[idx][0], range_max=p_range[idx][1],
+                                       resolution=p_res[idx], sigma=p_std[idx], mean=p_mean[idx]))
+
+prior_set = ParameterSet(*prior_params)
+prior_set.batch_len = batch_size
+if batch_size != None:
+    prior_set.isBatch = True
+else:
+    prior_set.isBatch = False
+prior_set.create_batch()
+
+# Create fixed params sampled from prior
+fixed_params = sampling_from_prior(prior_set, fixed_param_num)
+
+# Save parameter informations
+# Create database for data
+database = tb.open_file("/Users/Dani/TDK/parameter_estim/stim_protocol2/combining_colored_expcos2_Ra_gpas/paramsetup.hdf5", mode="w")
+
+# Save param initialization
+param_init = []
+for param in prior_set.params:
+    param_init.append(param.get_init())
+param_init = np.array(param_init, dtype=str)
+
+database.create_array(database.root, "params_init",
+                      title="Parameter space initializer. True value is about to set up!",
+                      atom=tb.Atom.from_dtype(param_init.dtype),
+                      shape=param_init.shape, obj=param_init)
+
+
+# Save fixed params and target_traces
+fixed_p = np.ndarray(shape=(len(fixed_params), len(prior_set.params)))
+for idx, item in enumerate(fixed_params):
+    for i, param in enumerate(prior_set.params):
+        fixed_p[idx, i] = item[param.name]
+
+database.create_array(database.root, "fixed_params",
+                      title="True value for each parameter in given simulation",
+                      atom=tb.Atom.from_dtype(fixed_p.dtype),
+                      shape=fixed_p.shape, obj=fixed_p)
+
+database.flush()
+print "Parameter space initialization data saved to disk"
+print database
+database.close()
+
+
+for item in hz:
+    print "\n\n---------------------------------------- Running %i Hz zap protocol" % item
+
+    # Stimulus path
+    stim = np.loadtxt("/Users/Dani/TDK/parameter_estim/stim_protocol2/stimulus/sin/%i/stim.txt" % item)
+    working_path = "/Users/Dani/TDK/parameter_estim/stim_protocol2/combining_colored_expcos2_Ra_gpas/sins/%i" % item
+
+    modell = partial(model, stim=stim)
+
+    # Generate synthetic data for each fixed params and given repetition
+    target_traces = more_trace_from_covmat(covmat, model=modell, params=fixed_params, rep=noise_rep)
+    print "The shape of target traces: " + str(target_traces.shape)
+
+    if __name__ == '__main__':
+        run_protocol_simulations_c(model=modell, target_traces=target_traces, inv_covmat=invcovmat, param_set=prior_set,
+                                 working_path=working_path)
+
+for item in duration:
+    print "\n\n---------------------------------------- Running %i ms impulse protocol" % item
+
+    # Stimulus path
+    stim = np.loadtxt("/Users/Dani/TDK/parameter_estim/stim_protocol2/stimulus/step/%i/stim.txt" % item)
+    working_path = "/Users/Dani/TDK/parameter_estim/stim_protocol2/combining_colored_expcos2_Ra_gpas/steps/%i" % item
+
+    modell = partial(model, stim=stim)
+
+    # Generate synthetic data for each fixed params and given repetition
+    target_traces = more_trace_from_covmat(covmat, model=modell, params=fixed_params, rep=noise_rep)
+
+    if __name__ == '__main__':
+        run_protocol_simulations_c(model=modell, target_traces=target_traces, inv_covmat=invcovmat, param_set=prior_set,
+                                 working_path=working_path)
+
+runningTime = (time.time()-startTime)/60
+print "\n\nThe script was running for %f minutes" % runningTime
